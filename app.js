6912881f39254a2b4126d67b982ae42be6573e0e
Alem42 <915987620@qq.com>
2024-05-11 17:17:42 +0800

    0.1.2

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(express.json()); // For Express 4.16.0 and higher
+
+const users = [
+  {username:'pp', password:'666'},
+  {username:'tt', password:'666'},
+]
+
+// 配置CORS策略
+const corsOptions = {
+  origin: 'http://localhost:5173', // 允许所有从localhost发起的请求
+};
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send('New App.js!\n');
 });
+
+// 定义一个POST路由
+app.post('/login', (req, res) => {
+  // 直接将接收到的请求体返回给客户端
+  console.log(req.body)
+  const user = users.find(u => u.username === req.body.username && u.password === req.body.password);
+  if (user) {
+    console.log("login_true");
+    res.send({
+      ...req.body,
+      ok: true
+    });
+  } else {
+    console.log("login_false");
+    res.status(401).send({ ok: false, message: '用户名或密码错误' });  // 使用明确的HTTP状态码和错误消息
+  }
+})
 
 const PORT = 3000;
 app.listen(PORT, () => {
